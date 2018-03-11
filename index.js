fc8a4205183b646e7f121f94464964510c89da4f
Vivek Kumar <vivek.kumar26@live.com>
2018-03-11 20:52:40 +0530

    feat(load-spec): parse and load api specification

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = {};
+/**
+ * @author Vivek Kumar <vivek.kumar26@live.com>
+ * @license MIT
+ */
+
+'use strict';
+const { OAIRouter } = require('./lib');
+module.exports = OAIRouter;
